9bd4cb8b16166dd7d1e83b93707a9c14a709cc09
kcbabo <340837+kcbabo@users.noreply.github.com>
2023-03-28 10:54:04 -0400

    add Portal REST API RT in init.sh

--- install/init.sh
+++ install/init.sh
@@ -6,3 +6,4 @@ kubectl apply -f policy/rl-server.yaml
 kubectl apply -f policy/rl-config.yaml
 kubectl apply -f policy/portal-cors.yaml
 kubectl apply -f api-example-com-rt.yaml
+kubectl apply -f developer-example-com-rt.yaml
