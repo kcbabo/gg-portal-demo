df184670c9a2d481ae336d23d4f50f8c7a498e6f
kcbabo <340837+kcbabo@users.noreply.github.com>
2023-04-13 17:32:11 -0400

    remove extraneous oidc config

--- install/init.sh
+++ install/init.sh
@@ -5,7 +5,6 @@ kubectl create ns tracks
 kubectl apply -f portal-frontend.yaml
 kubectl apply -f policy/api-key.yaml
 kubectl apply -f policy/auth-server.yaml
-kubectl apply -f policy/oidc.yaml
 kubectl apply -f policy/rl-server.yaml
 kubectl apply -f policy/rl-config.yaml
 kubectl apply -f policy/portal-cors.yaml
