ea0745425957e7d65c328454d28409fdc8f317ab
Duncan Doyle <Duncan.Doyle@gmail.com>
2024-02-06 12:27:22 +0100

    Defaulting to a single workspace.

--- install/init.sh
+++ install/init.sh
@@ -7,17 +7,17 @@ kubectl create ns petstore
 
 # Either create the 4 different workspaces (gateways, keycloak, tracks and petstore), or create the single-default workspace.
 ########################################################################################
-kubectl apply -f workspaces/gateways-workspace.yaml
-kubectl apply -f workspaces/gateways-workspacesettings.yaml
-kubectl apply -f workspaces/keycloak-workspace.yaml
-kubectl apply -f workspaces/keycloak-workspacesettings.yaml
-kubectl apply -f workspaces/petstore-workspace.yaml
-kubectl apply -f workspaces/petstore-workspacesettings.yaml
-kubectl apply -f workspaces/tracks-workspace.yaml
-kubectl apply -f workspaces/tracks-workspacesettings.yaml
+# kubectl apply -f workspaces/gateways-workspace.yaml
+# kubectl apply -f workspaces/gateways-workspacesettings.yaml
+# kubectl apply -f workspaces/keycloak-workspace.yaml
+# kubectl apply -f workspaces/keycloak-workspacesettings.yaml
+# kubectl apply -f workspaces/petstore-workspace.yaml
+# kubectl apply -f workspaces/petstore-workspacesettings.yaml
+# kubectl apply -f workspaces/tracks-workspace.yaml
+# kubectl apply -f workspaces/tracks-workspacesettings.yaml
 ########################################################################################
-# kubectl apply -f workspaces/gg-demo-single-default-workspace.yaml
-# kubectl apply -f workspaces/gg-demo-single-default-workspacesettings.yaml
+kubectl apply -f workspaces/gg-demo-single-default-workspace.yaml
+kubectl apply -f workspaces/gg-demo-single-default-workspacesettings.yaml
 ########################################################################################
 
 kubectl apply -f portal-frontend.yaml
